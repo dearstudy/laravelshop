2a2cba24348331632e6f7d3d343d0b38537269e5
vini123 <vini123@sina.cn>
2019-01-14 03:31:02 +0000

    新增收货地址

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,7 @@ Route::get('/', 'PageController@root')->name('root')->middleware('verified');
 Auth::routes(['verify' => true]);
 
 Route::group(['middleware' => ['auth', 'verified']], function() {
-    Route::get('user_addresses', 'UserAddressController@index')->name('user_address.index');
+    Route::get('user_addresses', 'UserAddressController@index')->name('user_addresses.index');
+    Route::get('user_addresses/create', 'UserAddressController@create')->name('user_addresses.create');
+    Route::post('user_addresses', 'UserAddressController@store')->name('user_addresses.store');
 });
